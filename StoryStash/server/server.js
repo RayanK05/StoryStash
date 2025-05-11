8b0ed47842fa40315b8054c7ab48baa7a846e056
Mohamed-Omar17 <mooniomar17@gmail.com>
2025-05-10 23:51:54 -0400

    Change whatever i highlighted in the notes section

--- StoryStash/server/server.js
+++ StoryStash/server/server.js
@@ -3,7 +3,7 @@ import pg from 'pg';
 
 const app = express();
 
-const db = new pg.Client({
+const db = new pg.Client({ //change the password, user, and database names to whatever you save them as 
   user: "postgres",
   host: "localhost",
   database: "media_notes",
@@ -13,17 +13,17 @@ const db = new pg.Client({
 db.connect();
 
 
-let user = "Mohamed";
+let user = "Mohamed"; //set this to the email you use to test
 
 
 app.get ("/api", async (req, res) => {
-  let result = await db.query(
+  let result = await db.query( //queries for the email in the user variable
     "SELECT users.email, media.media_id, media.title AS media_title, media.media_type, notes.title AS note_title, notes.note FROM users JOIN media ON users.email = media.user_email JOIN notes ON media.media_id = notes.media_id WHERE users.email = $1", [user]);
-  let items = [];
+  let items = []; //appends each item from that user into the items array
   result.rows.forEach((item) => items.push({media: item.media_title, type: item.media_type, title: item.note_title, note : item.note}));
   
   console.log(items); // Check what's being sent
-res.json({ postData: items });
+  res.json({ postData: items });
 })
 
 app.listen(3100, () => {
